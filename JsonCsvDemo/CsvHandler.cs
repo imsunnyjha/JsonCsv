abbf0bfc4d048d28175c77a9c3e9b6584ac4afbc
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-26 09:42:13 +0530

    [SUNNY]Add.Csv import and export

---
+++ JsonCsvDemo/CsvHandler.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JsonCsvDemo
+{
+    class CsvHandler
+    {
+        public static void ImplementCSVDataHandling()
+        {
+            string importFilePath = @"C:\Users\lenovo\source\repos\JsonCsvDemo\JsonCsvDemo\Address.csv";
+            string exportFilePath = @"C:\Users\lenovo\source\repos\JsonCsvDemo\JsonCsvDemo\export.csv";
+
+            //reading file
+            using (var reader=new StreamReader(importFilePath))
+            using (var csv=new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data successfully from address.csv");
+                foreach(AddressData addressData in records)
+                {
+                    Console.WriteLine("\t" + addressData.firstname);
+                    Console.WriteLine("\t" + addressData.lastname);
+                    Console.WriteLine("\t" + addressData.address);
+                    Console.WriteLine("\t" + addressData.city);
+                    Console.WriteLine("\t" + addressData.state);
+                    Console.WriteLine("\t" + addressData.code);
+                    Console.WriteLine("\n");
+                }
+                Console.WriteLine("\t%%%%%%%%%%%%%%%% Now reading from csv file and write to csv file %%%%%%%%%%%%%%");
+
+
+                //writing csv file
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var csvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvExport.WriteRecords(records);
+                }
+            }
+        }
+    }
+}
---
+++ JsonCsvDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JsonCsvDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read data from csv>>>>>>>>>");
+            CsvHandler.ImplementCSVDataHandling();
+
+        }
+    }
+}
